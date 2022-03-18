e66e3a2b1ceb67f98e5030949b04085d493c34de
calmkeen <iyunseong159@gmail.com>
2022-03-18 14:10:51 +0900

    Initial Commit

---
+++ LoadMap_CLLocation/LoadMap_CLLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadMap_CLLocationApp.swift
+//  LoadMap_CLLocation
+//
+//  Created by calmkeen on 2022/03/18.
+//
+
+import SwiftUI
+
+@main
+struct LoadMap_CLLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
